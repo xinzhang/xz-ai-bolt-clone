7dd3f0f8ddcf4807a3f1f0cc64eec92a6a865b13
xinzhang <xin_zhang_2000@yahoo.com>
2025-05-20 20:59:53 +1000

    clean up

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -15,14 +15,17 @@ import { MessageCircleIcon } from 'lucide-react'
 import WorkspaceHistory from './WorkspaceHistory'
 import Footer from './Footer'
 import { useSidebar } from './ui/sidebar'
+import { useRouter } from 'next/navigation'
 
 const AppSidebar = () => {
   const { toggleSidebar } = useSidebar();
+  const router = useRouter();
+  
   return (
     <Sidebar>
       <SidebarHeader className='p-5'>
         <Image onClick={() => toggleSidebar()} src={'/logo.png'} alt='logo' width={30} height={30} />
-        <Button className='mt-5'><MessageCircleIcon className='w-4 h-4 mr-2' /> Start new chat</Button>
+        <Button className='mt-5 cursor-pointer' onClick={() => {toggleSidebar(); router.push('/')}}><MessageCircleIcon className='w-4 h-4 mr-2' /> Start new chat</Button>
       </SidebarHeader>
       <SidebarContent className='p-5'>        
         <SidebarGroup>
